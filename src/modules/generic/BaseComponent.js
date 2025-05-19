927bf05bf8130a725e7fef7fabf28d486f9842a2
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2025-05-19 12:06:24 +0300

    finish component Tabs

---
+++ src/modules/generic/BaseComponent.js
@@ -0,0 +1,35 @@
+class BaseComponent {
+	constructor() {
+		if (this.constructor === BaseComponent) {
+			throw new Error('Невохможно создать экземпляр обстрактного класса BaseComponent!')
+		}
+	}
+	
+	getProxyState(initialState) {
+		return new Proxy(initialState, {
+			get: (target, prop) => {
+				return target[prop]
+			},
+			set: (target, prop, newValue) => {
+				const oldValue = target[prop]
+				
+				target[prop] = newValue
+				
+				if (newValue !== oldValue) {
+					this.updateUI()
+				}
+				
+				return true
+			}
+		})
+	}
+	
+	/**
+	 * Перерисовка UI в ответ на обновления состояния
+	 */
+	updateUI() {
+		throw new Error('Необходимо реализовать метод updateUI!')
+	}
+}
+
+export default BaseComponent;
