9f226ff93d2c60e90cf18c918bd3be0c770a6be1
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2025-05-21 14:21:15 +0300

    comp EpisodeCard

--- src/components/Seasons/Seasons.jsx
+++ src/components/Seasons/Seasons.jsx
@@ -2,11 +2,13 @@ import './Seasons.scss';
 import AccordionGroup from '@/components/AccordionGroup';
 import seasonItems from '@/components/Seasons/seasonItems';
 import Accordion from '@/components/Accordion';
+import EpisodeCard from '@/components/EpisodeCard';
 
 const Seasons = (props) => {
 	
 	return (
 		<AccordionGroup
+			className='seasons'
 			mode='dark'
 			isOrderedList={false}
 		>
@@ -21,7 +23,13 @@ const Seasons = (props) => {
 					key={index}
 					isArrowButton
 				>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, adipisci, aliquam consequatur cumque dolorem et hic illum ipsam iure labore laborum nam, nisi numquam officiis perspiciatis quis reiciendis sapiente suscipit?
+					<ul className='seasons__list'>
+						{episodes.map((episode, index) => (
+							<li className='seasons__item' key={index}>
+								<EpisodeCard {...episode}/>
+							</li>
+						))}
+					</ul>
 				</Accordion>
 			))}
 		</AccordionGroup>
--- src/components/Seasons/seasonItems.js
+++ src/components/Seasons/seasonItems.js
@@ -5,92 +5,22 @@ const seasonItems = [
 		episodes: [
 			{
 				number: '01',
-				title: '',
-				description: '',
+				title: 'Chapter One : The Vanishing of Will Byers',
+				description: 'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
 				duration: '49 min',
 				video: {
 					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
+					poster: '/video-posters/1.jpg'
 				}
 			},
 			{
 				number: '01',
-				title: '',
-				description: '',
+				title: 'Chapter One : The Vanishing of Will Byers',
+				description: 'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
 				duration: '49 min',
 				video: {
 					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
+					poster: '/video-posters/1.jpg'
 				}
 			},
 		]
@@ -101,188 +31,48 @@ const seasonItems = [
 		episodes: [
 			{
 				number: '01',
-				title: '',
-				description: '',
+				title: 'Chapter One : The Vanishing of Will Byers',
+				description: 'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
 				duration: '49 min',
 				video: {
 					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
+					poster: '/video-posters/1.jpg'
 				}
 			},
 			{
 				number: '01',
-				title: '',
-				description: '',
+				title: 'Chapter One : The Vanishing of Will Byers',
+				description: 'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
 				duration: '49 min',
 				video: {
 					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
+					poster: '/video-posters/1.jpg'
 				}
 			},
 		]
 	},
 	{
 		title: 'Season 03',
-		subtitle: '2 Episodes',
+		subtitle: '5 Episodes',
 		episodes: [
 			{
 				number: '01',
-				title: '',
-				description: '',
+				title: 'Chapter One : The Vanishing of Will Byers',
+				description: 'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
 				duration: '49 min',
 				video: {
 					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
+					poster: '/video-posters/1.jpg'
 				}
 			},
 			{
 				number: '01',
-				title: '',
-				description: '',
+				title: 'Chapter One : The Vanishing of Will Byers',
+				description: 'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
 				duration: '49 min',
 				video: {
 					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
-				}
-			},
-			{
-				number: '01',
-				title: '',
-				description: '',
-				duration: '49 min',
-				video: {
-					src: '/videos/example.mp4',
-					poster: '/video-poster/1.jpg'
+					poster: '/video-posters/1.jpg'
 				}
 			},
 		]
